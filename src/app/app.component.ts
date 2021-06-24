e4cf6f427eef1f660dd3f9c8aee26a8a918afc8d
pandula <pandula95@gmail.com>
2021-06-24 13:43:34 +0530

    feat: Render Outputs in sumbitted via quill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,19 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   editform: FormGroup;
+  editorStyle = {
+    height: '300px;',
+  };
+  editorContent;
 
   ngOnInit() {
     this.editform = new FormGroup({
       editor: new FormControl(null),
     });
+  }
+
+  onSubmit() {
+    this.editorContent = this.editform.get('editor').value;
+    console.log(this.editform.get('editor').value);
   }
 }
